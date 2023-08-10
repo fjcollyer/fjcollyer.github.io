2df15741d535a1312dfa2b5397af0bf844fafb3f
JamesCollyer <collyerfj@gmail.com>
2023-08-10 01:39:00 +0100

    spinner bug on game end fixed4

--- connect4/script.js
+++ connect4/script.js
@@ -259,8 +259,6 @@ function updateTurnMessage() {
     if(!statusMessageElement) return; 
     const statusMessageElement = document.getElementById("statusText");
     const spinnerElement = document.getElementById("spinner");
-    if(!statusMessageElement) return; 
-    if(!spinnerElement) return;
     if(spinnerElement) {
         spinnerElement.style.display = "none";
     }
